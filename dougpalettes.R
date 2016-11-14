8dcfd8e634e49361fd49efb2d124ee80205c976e
dougmcneall <dougmcneall@users.noreply.github.com>
2016-11-14 14:30:37 +0000

    Add files via upload

---
+++ dougpalettes.R
@@ -0,0 +1,10 @@
+## useful palettes
+
+
+## 17 different colours for line plots
+dougpal1 <- colors()[c(24,552,26,81,90,82,32,142,31,50,11,116,74,150,101,102,508 )]
+ 
+dougpal2 <- colors()[c(24,552,26,208,90,81,32,142,31,50,11,116,74,150,101,102,508 )]
+
+
+dougpal3 <- c('black', 'red', 'dodgerblue',' darkgrey', 'green3', 'gold2')
---
+++ dougplotpars.R
@@ -0,0 +1,22 @@
+## dougplotpars.R
+
+# setting up useful plot defaults
+
+dougpar1 <- list(mar = c(5,5,4,2),
+                font.main = 1,
+                bty = 'l',
+                tcl = 0.5,
+                mgp = c(2.3,0.3,0)
+                )
+
+
+dougpar_web<- list(mar = c(5,5,4,2),
+                   font.main = 1,
+                   bty = 'l',
+                   tcl = 0.5,
+                   mgp = c(2.5,0.5,0),
+                   las = 1,
+                   fg = 'darkgrey',
+                   cex.lab = 1.5,
+                   cex.axis = 1.5
+                )
